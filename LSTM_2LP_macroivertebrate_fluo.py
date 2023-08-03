0efd14c38274a4dbd4f1ee11fcf00b0482fa48f8
max-talanov <max.talanov@gmail.com>
2023-08-03 15:27:19 +0200

    completed lstm

--- LSTM_2LP_macroivertebrate_fluo.py
+++ LSTM_2LP_macroivertebrate_fluo.py
@@ -11,19 +11,19 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error
 
 data = pd.read_csv(r"../Ksenobiotici_makroinvertebrate.csv",encoding='latin-1')
-## Bromacil
-X = data.iloc[2:,[73,69,68,70,72,58,60,91]].values  
-y = data.iloc[2:,[166]].values 
-## 2.4 D
+#print("Bromacil")
+#X = data.iloc[2:,[73,69,68,70,72,58,60,91]].values  
+#y = data.iloc[2:,[166]].values 
+#print("2.4 D")
 #X = data.iloc[2:,[64,55,93,111,116,35,161,162]].values  
 #y = data.iloc[2:,[164]].values 
-## Fluoranthene 
+#print("Fluoranthene")
 #X = data.iloc[2:,[79,80,64,76,89,88,83]].values  
 #y = data.iloc[2:,[170]].values 
-## Fluoranthene & 2.4 D 
-#X = data.iloc[2:,[79,80,64,76,89,88,83]].values  
-#y = data.iloc[2:,[170,164]].values 
-## Bentazone
+print("Fluoranthene & 2.4 D")
+X = data.iloc[2:,[79,80,64,76,89,88,83]].values  
+y = data.iloc[2:,[170,164]].values 
+#print("Bentazone")
 #X = data.iloc[2:,[64,45,89,88,83,107,90,93,91]].values  
 #y = data.iloc[2:,[169]].values 
 
@@ -32,10 +32,10 @@ y=y.astype("float32")
 
 ## logging
 ##print(X)
-print(X.shape)
+#print(X.shape)
 ##print (y)
-print(y.shape)
-print(len(y))
+#print(y.shape)
+#print(len(y))
 
 train_index=np.arange(0, X.shape[0])#train index all indexes
 test_index= np.random.randint(0, X.shape[0],2)#random 10 for test
@@ -56,7 +56,7 @@ model.add(LSTM(2))
 #model.add(Dense(8, activation='sigmoid'))
 #model.add(Dense(12, activation='sigmoid'))
 #model.add(Dense(30, activation='sigmoid'))
-model.add(Dense(1, activation='linear'))
+model.add(Dense(2, activation='linear'))
 
 tf.random.set_seed(12345)
 model.compile(loss='mean_squared_error', optimizer=tf.keras.optimizers.Adam(0.001))
@@ -96,4 +96,26 @@ y_pred = model.predict(X_test)
 # calculate MSE
 mse = mean_squared_error(y_test, y_pred)
 # print the result
-print("MSE:", mse)
+print("MSE=", mse)
+
+## Bromacil
+# 0.001426385
+# 0.0031136826
+# 0.0017592615
+## 2.4 D
+# 1.36419485e-05
+# 3.263747e-05
+# 8.033701e-06
+## Fluoranthene
+# 0.0077476795
+# 8.323929e-08
+# 0.0037221021
+## Bentazone
+# 0.00057681283
+# 3.9730803e-06
+# 1.6001328e-05
+## Fluoranthene & 2.4 D
+# 3.3694337e-07
+# 3.5156765e-05
+# 0.0001348218
+
